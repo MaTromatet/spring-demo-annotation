90e87920ae334b14e237724d364aec01549fa24c
MaTromatet <mromatet@gmail.com>
2022-04-13 11:30:40 +0200

    Bean Scopes with Annotations + Exos lecture de .txt en @PostConstruct

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,17 +1,23 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach")
 @Component
+//pour changer le scope
+// @Scope("prototype")
 public class TennisCoach implements Coach {
 
 	//Field injection 
 	@Autowired
 	//définir quelle implémentation de l'interface va etre utilisée
-	@Qualifier("randomFortuneService")
+	@Qualifier("fileFortuneService")
 	private FortuneService fortuneService; 
 	
 	//-----------------------------------------------------
@@ -65,8 +71,21 @@ public class TennisCoach implements Coach {
 	}
 	
 	*/
+	//-----------------------------------------------------
 	
-	 //-----------------------------------------------------
+	// define my init method
+		@PostConstruct
+		public void doMyStartupStuff() {
+			System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+		}
+		
+		// define my destroy method
+		@PreDestroy
+		public void doMyCleanupStuff() {
+			System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");		
+		}
+	
+	 
 	 //methode
 	   
 	@Override
